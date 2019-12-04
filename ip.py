9f2049272f5ba0ec8f65995ed574b829f1b7eee8
Greg Risch <gprisch@ideogeneteic.com>
2019-12-04 15:04:57 +0100

    played with IP numbers

--- ip.py
+++ ip.py
@@ -1,3 +1,13 @@
 user_input = '192.168.1.12'
 ip = '.'.join([f'{int(n):08b}' for n in user_input.split('.')])
 print(ip)
+
+# functions
+a_bin = bin(192)
+a_oct = oct(192)
+a_hex = hex(192)
+print("192: b:%s o:%s h:%s" % (a_bin, a_oct, a_hex))
+
+print(int(0b11000000))
+print(int(0o300))
+print(int(0xc0))
