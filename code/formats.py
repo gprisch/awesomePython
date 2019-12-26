85173065991535eb1d0f7db0d673785eb55c6c3e
Greg Risch <gprisch@ideogeneteic.com>
2019-12-27 00:10:45 +0100

    added a couple of string, numbers formatting

---
+++ code/formats.py
@@ -0,0 +1,36 @@
+# string.format()
+a_str = "|{1!r:10} loves {0!r:10}|"
+formatted_1 = a_str.format('Greg', 'Jean')
+print(formatted_1)
+
+another_str = "|{1!s:10} loves {0!s:10}|"
+formatted_2 = another_str.format('Greg', 'Jean')
+print(formatted_2)
+
+str3 = '{:-^25}'  # center
+str4 = '{:->25}'  # right
+str5 = '{:-<25}'  # left
+print(str3.format('Hello world'))
+print(str4.format('Hello world'))
+print(str5.format('Hello world'))
+
+# for numeric values
+print('{:=10}'.format(-12))
+print('{:0=10}'.format(-1245))
+
+# all examples can be executed like this
+print(format('Lady','@<10'))
+
+# blank, ) or -, just minuses
+print('results >{: 10},{:+10},{:-10}'.format(25, 25, 25))
+
+# large numbers, trailing 0
+large1, large2 = 18293044944764648, -10_000_980
+print('{:,}'.format(large1))
+print('{:-018,}'.format(large2))
+
+# not trailing 0 in the
+# next but a group of 0
+print('{:0>10}'.format(-16))
+
+
