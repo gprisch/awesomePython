7fd304bdb4c116a6a9e6ede490c6ed321f94d343
Greg Risch <gprisch@ideogeneteic.com>
2019-12-28 21:22:11 +0100

    had a quick look on the termcolor module

--- code/reguexp_1.py
+++ code/reguexp_1.py
@@ -1,10 +1,20 @@
 import re
 
-mobile = '06-09-00-15-45'
+from termcolor import cprint, colored, COLORS
 
+mobile = '06 09-00-15-45'
+
+# raw string
 pattern1 = r'\d\d-\d\d-\d\d-\d\d-\d\d'
+# add end of line $ ans start of line ^
+pattern2 = r'^\d\d-\d\d-\d\d-\d\d-\d\d$'
+pattern3 = r'^[0-9]{2}[ -]\d\d-\d\d-\d\d-\d\d$'
 
-if re.match(pattern1, mobile):
-    print(f'{mobile} is accepted')
+for k, v in COLORS.items():
+    print(f'{k}', end='  ')
+print('\n')
+
+if re.fullmatch(pattern3, mobile):
+    cprint(f'{mobile} is accepted', 'green', on_color='on_white', attrs=['bold'])
 else:
-    print(f'{mobile} is rejected')
+    print(f'{mobile} is {colored("rejected", "grey", "on_red")}')
