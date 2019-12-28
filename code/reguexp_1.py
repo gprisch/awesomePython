b8289e84baf2be8364d0919a6b1c53a4693f41fd
Greg Risch <gprisch@ideogeneteic.com>
2019-12-28 23:49:35 +0100

    about to review the regexp search

--- code/reguexp_1.py
+++ code/reguexp_1.py
@@ -7,14 +7,42 @@ mobile = '06 09-00-15-45'
 # raw string
 pattern1 = r'\d\d-\d\d-\d\d-\d\d-\d\d'
 # add end of line $ ans start of line ^
-pattern2 = r'^\d\d-\d\d-\d\d-\d\d-\d\d$'
-pattern3 = r'^[0-9]{2}[ -]\d\d-\d\d-\d\d-\d\d$'
+pattern2 = r'\A\d\d-\d\d-\d\d-\d\d-\d\d$'
+pattern3 = r'\A[0-9]{2}[ -]\d\d-\d\d-\d\d-\d\d$'
+pattern4 = r'\A[0-9]{2}[ -]\d{2}-\d{2}-\d{2}-\d{2}'
 
 for k, v in COLORS.items():
     print(f'{k}', end='  ')
 print('\n')
 
-if re.fullmatch(pattern3, mobile):
+if re.fullmatch(pattern4, mobile):
     cprint(f'{mobile} is accepted', 'green', on_color='on_white', attrs=['bold'])
 else:
     print(f'{mobile} is {colored("rejected", "grey", "on_red")}')
+
+a_str = 'this is'
+
+
+def test_exp(p):
+    global a_str
+    if re.match(p, a_str):
+        print('match')
+    else:
+        print('no match')
+
+
+# start of line
+pattern = r'\At'
+test_exp(pattern)
+# word boundaries
+pattern = r'\b.+s\si*'
+test_exp(pattern)
+# test presence of one digit
+
+res1 = re.match(r'\w*\d', 'aaaaajdld9hhhh')
+assert res1 is not None
+
+res2 = re.match(r'g(r+)(ego)(r.*)', 'grrrrrrrrrrregoryyyyy')
+for i in range(res2.lastindex + 1):
+    print(f'{i}:{res2.group(i)}', sep=' ')
+
